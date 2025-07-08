72818a192546fc22d0033a6dfc09895944bb6a76
orion-3464 <nikolaoskor8@gmail.com>
2025-07-08 15:27:04 +0300

    added indentation and scrolling options

--- lua/orion3464/options.lua
+++ lua/orion3464/options.lua
@@ -1,6 +1,22 @@
-local o = vim.o
+local opt = vim.opt
 
--- Editor UI
-o.number = true
-o.numberwidth = 5
-o.relativenumber = true
+-- Editor UI options
+opt.number = true
+opt.numberwidth = 5
+opt.relativenumber = true
+
+-- Indentation options
+opt.shiftwidth = 4
+opt.smarttab = true
+
+-- Scrolling options
+opt.scrolloff = 999
+opt.wrap = false
+
+-- Search options
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.history = 1000
